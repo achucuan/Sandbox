b44193bce082e11895b2a59bce94c6921b8ed352
Alberto Chucuan <achucuan@picmonic.com>
2014-11-27 18:20:29 -0600

    #### Description - Bower init created - html basic structure - dir sctruct #### To install run <bower install>

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    /* Used to toggle #sidebar */
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+});
